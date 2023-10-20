1a0a563971aa7b5e0aada14f627c4737c09f5c42
Pcao82 <144163052+Pcao82@users.noreply.github.com>
2023-10-20 01:32:28 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace C__with_Class
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Labelresults = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.zipcode = new System.Windows.Forms.Label();
+            this.txtZipcode = new System.Windows.Forms.TextBox();
+            this.listBoxResults = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(147, 47);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(167, 20);
+            this.txtName.TabIndex = 1;
+            this.txtName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(353, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Labelresults
+            // 
+            this.Labelresults.AutoSize = true;
+            this.Labelresults.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Labelresults.Location = new System.Drawing.Point(223, 341);
+            this.Labelresults.Name = "Labelresults";
+            this.Labelresults.Size = new System.Drawing.Size(49, 13);
+            this.Labelresults.TabIndex = 3;
+            this.Labelresults.Text = "Results";
+            this.Labelresults.Click += new System.EventHandler(this.Labelresults_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(68, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Address";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(147, 109);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(167, 20);
+            this.txtAddress.TabIndex = 6;
+            this.txtAddress.TextChanged += new System.EventHandler(this.txtAddress_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(68, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "City";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(147, 162);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(167, 20);
+            this.txtCity.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(61, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "State";
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(147, 217);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(167, 20);
+            this.txtState.TabIndex = 10;
+            // 
+            // zipcode
+            // 
+            this.zipcode.AutoSize = true;
+            this.zipcode.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.zipcode.Location = new System.Drawing.Point(61, 277);
+            this.zipcode.Name = "zipcode";
+            this.zipcode.Size = new System.Drawing.Size(58, 13);
+            this.zipcode.TabIndex = 11;
+            this.zipcode.Text = "Zip Code";
+            // 
+            // txtZipcode
+            // 
+            this.txtZipcode.Location = new System.Drawing.Point(147, 270);
+            this.txtZipcode.Name = "txtZipcode";
+            this.txtZipcode.Size = new System.Drawing.Size(167, 20);
+            this.txtZipcode.TabIndex = 12;
+            // 
+            // listBoxResults
+            // 
+            this.listBoxResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxResults.FormattingEnabled = true;
+            this.listBoxResults.Location = new System.Drawing.Point(398, 169);
+            this.listBoxResults.Name = "listBoxResults";
+            this.listBoxResults.Size = new System.Drawing.Size(189, 160);
+            this.listBoxResults.TabIndex = 13;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxResults);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtZipcode);
+            this.Controls.Add(this.zipcode);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Labelresults);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Labelresults;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.Label zipcode;
+        private System.Windows.Forms.TextBox txtZipcode;
+        private System.Windows.Forms.ListBox listBoxResults;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C__with_Class
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        class Person 
+        {
+            private string name;
+            private string address;
+            private string city;
+            private string state;
+            private string zipcode;
+
+            public string Name
+            {
+                get { return name;}
+                set { name = value;}
+            }
+            public string Address
+            {
+                get { return address; }
+                set { address = value; }
+            }
+
+                public string City
+            {
+                get { return city; }
+                set { city = value; }
+                }
+
+            public string Zipcode
+            {
+                get { return zipcode;}
+                set { zipcode = value;}
+               
+            }
+
+            public string State
+            {
+                get { return state;}
+                set { state = value; }
+            }
+
+            
+
+
+
+        
+        }
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Labelresults_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Person personobject = new Person();
+            personobject.Name = txtName.Text;
+            personobject.Address = txtAddress.Text;
+            personobject.State = txtState.Text;
+            personobject.City = txtCity.Text;
+            personobject.Zipcode = txtZipcode.Text;
+
+            listBoxResults.Items.Add(personobject.Name);
+            listBoxResults.Items.Add(personobject.Address);
+            listBoxResults.Items.Add(personobject.City);
+            listBoxResults.Items.Add(personobject.State);
+            listBoxResults.Items.Add(personobject.Zipcode);
+
+
+        }
+
+        private void Textname_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAddress_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
